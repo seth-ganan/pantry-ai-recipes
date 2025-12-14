5e2aea052fe3962b25cd60bc8532e3dd21ea55e2
Seth Ganan <seth.ganan@drake.edu>
2025-12-14 15:41:41 -0600

    ahhhhh

--- backend/utilities/openai.js
+++ backend/utilities/openai.js
@@ -1,10 +1,10 @@
-const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new OpenAI.Configuration({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-const openai = new OpenAI.OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
 async function standardizeIngredient(name, amount) {
   try {
